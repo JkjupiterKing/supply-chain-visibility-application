73c8bdabcc57a2599ce1326ca373258ba7345c77
JK <jupiterkingtechnologies@gmail.com>
2024-07-08 17:16:36 +0530

    added the logout button functionality to supplier files.

--- app/supplier-management/supplier-management.js
+++ app/supplier-management/supplier-management.js
@@ -216,3 +216,8 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+// JavaScript for handling logout button click
+document.getElementById('logoutBtn').addEventListener('click', function() {
+    // Redirect to login page
+    window.location.href = '/app/Login/login.html'; // Replace with your actual login page URL
+});
