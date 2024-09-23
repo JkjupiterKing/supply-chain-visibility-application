c55335d3c45d9111dbef74b16cab48a4d688b47e
JK <jupiterkingtechnologies@gmail.com>
2024-09-23 18:12:47 +0530

    Modified the Customerlogin, login modules.

--- app/Customerlogin/Customerlogin.js
+++ app/Customerlogin/Customerlogin.js
@@ -1,62 +1,98 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     // Function to handle login button click
-//     document.getElementById("submit").addEventListener("click", function() {
-//         var username = document.getElementById("username");
-//         var password = document.getElementById("password");
+document.addEventListener("DOMContentLoaded", function () {
+  const submitButton = document.getElementById("submit");
 
-//         if (username !== "" && password !== "") {
-//             var isLoggedIn = true; 
+  submitButton.addEventListener("click", function (event) {
+    event.preventDefault();
 
-//             if (isLoggedIn) {
-//                 // Store login success message in local storage
-//                 localStorage.setItem("loginMessage", "Login successful.");
-//                 // Redirect to the cart page
-//                 window.location.href = "/app/components/cart/cart.html";
-//             } else {
-//                 alert("Login failed. Please check your credentials.");
-//             }
-//         } else {
-//             alert("Please enter username and password.");
-//         }
-//     });
-// });
+    // Retrieve input field data
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
 
-document.addEventListener('DOMContentLoaded', function () {
-    const submitButton = document.getElementById('submit');
+    // Make the GET request to the API endpoint to fetch user by username
+    fetch(`http://localhost:8080/users/username/${username}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((user) => {
+        if (user && user.password) {
+          const decodedPassword = atob(user.password);
+          if (password === decodedPassword) {
+            localStorage.setItem("loginMessage", "Login successful!");
+            localStorage.setItem("username", username);
+            window.location.href = "../index/index.html";
+          } else {
+            alert("Invalid username or password.");
+          }
+        } else {
+          alert("User not found.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("An error occurred. Please try again.");
+      });
+  });
 
-    submitButton.addEventListener('click', function (event) {
-        event.preventDefault(); 
+  const forgotPasswordLink = document.querySelector(".forgot-password-link");
+  const modal = new bootstrap.Modal(
+    document.getElementById("forgotPasswordModal")
+  );
 
-        // Retrieve input field data
-        const username = document.getElementById('username').value.trim();
-        const password = document.getElementById('password').value.trim();
+  forgotPasswordLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    modal.show();
+  });
 
-        // Make the GET request to the API endpoint to fetch user by username
-        fetch(`http://localhost:8080/users/username/${username}`, { 
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
+  document
+    .getElementById("forgotPasswordForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      // Get the username and new password entered by the user
+      var username = document.getElementById("Username").value.trim();
+      var newPassword = document.getElementById("newPassword").value.trim();
+
+      // Ensure both fields are filled
+      if (!username || !newPassword) {
+        alert("Please enter both username and new password.");
+        return;
+      }
+
+      // Send a request to your backend API to handle password reset
+      var resetPasswordUrl = "http://localhost:8080/users/reset-password";
+      fetch(resetPasswordUrl, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username: username, password: newPassword }),
+      })
+        .then((response) => {
+          if (response.status === 404) {
+            alert(
+              "Username not found in the system, please enter a valid username."
+            );
+          } else if (response.ok) {
+            alert(
+              "Password has been reset. Please log in using the new password."
+            );
+            modal.hide(); // Close modal on success
+          } else {
+            alert("Failed to reset password. Please try again.");
+            modal.hide();
+          }
         })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(user => {
-            const decodedPassword = atob(user.password); 
-            if (password === decodedPassword) {
-                localStorage.setItem("loginMessage", "login successfull!!");
-                localStorage.setItem("username", username);
-                window.location.href = '../index/index.html'; 
-            } else {
-                alert('Invalid username or password.');
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert('An error occurred. Please try again.');
+        .catch((error) => {
+          console.error("Error:", error);
+          alert("Failed to reset password. Please try again.");
         });
     });
 });
--- app/Login/login.js
+++ app/Login/login.js
@@ -1,41 +1,96 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var loginForm = document.getElementById('login-form');
-    if (loginForm) {
-        loginForm.addEventListener('submit', function(event) {
-            event.preventDefault(); 
-            
-            // Retrieve username and password input values
-            var usernameInput = document.getElementById('username');
-            var passwordInput = document.getElementById('password');
-            
-            // Check if username and password are not empty
-            var username = usernameInput.value.trim();
-            var password = passwordInput.value.trim();
-            
-            if (username !== '' && password !== '') {
-                fetch(`http://localhost:8080/getUserByUsername/${username}`)
-                    .then(response => response.json())
-                    .then(data => {
-                        if (data && data.password) {
-                            var decodedPassword = atob(data.password);
-                            if (decodedPassword === password) {
-                                window.location.href = '../../app/dashboard/dashboard.html';
-                            } else {
-                                alert('Invalid username or password.');
-                            }
-                        } else {
-                            alert('User not found.');
-                        }
-                    })
-                    .catch(error => {
-                        console.error('Error fetching user data:', error);
-                        alert('Please enter valid username and password.');
-                    });
+document.addEventListener("DOMContentLoaded", function () {
+  var loginForm = document.getElementById("login-form");
+  if (loginForm) {
+    loginForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      // Retrieve username and password input values
+      var usernameInput = document.getElementById("username");
+      var passwordInput = document.getElementById("password");
+
+      // Check if username and password are not empty
+      var username = usernameInput.value.trim();
+      var password = passwordInput.value.trim();
+
+      if (username !== "" && password !== "") {
+        fetch(`http://localhost:8080/getUserByUsername/${username}`)
+          .then((response) => response.json())
+          .then((data) => {
+            if (data && data.password) {
+              var decodedPassword = atob(data.password);
+              if (decodedPassword === password) {
+                window.location.href = "../../app/dashboard/dashboard.html";
+              } else {
+                alert("Invalid username or password.");
+              }
             } else {
-                alert('Please enter both username and password.');
+              alert("User not found.");
             }
+          })
+          .catch((error) => {
+            console.error("Error fetching user data:", error);
+            alert("Please enter valid username and password.");
+          });
+      } else {
+        alert("Please enter both username and password.");
+      }
+    });
+  } else {
+    console.error("Could not find loginForm element.");
+  }
+
+  const forgotPasswordLink = document.querySelector(".forgot-password-link");
+  const modal = new bootstrap.Modal(
+    document.getElementById("forgotPasswordModal")
+  );
+
+  forgotPasswordLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    modal.show();
+  });
+
+  document
+    .getElementById("forgotPasswordForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      // Get the username and new password entered by the user
+      var username = document.getElementById("Username").value.trim();
+      var newPassword = document.getElementById("newPassword").value.trim();
+
+      // Ensure both fields are filled
+      if (!username || !newPassword) {
+        alert("Please enter both username and new password.");
+        return;
+      }
+
+      // Send a request to your backend API to handle password reset
+      var resetPasswordUrl = "http://localhost:8080/reset-password";
+      fetch(resetPasswordUrl, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username: username, password: newPassword }),
+      })
+        .then((response) => {
+          if (response.status === 404) {
+            alert(
+              "Username not found in the system, please enter a valid username."
+            );
+          } else if (response.ok) {
+            alert(
+              "Password has been reset. Please login using the new password."
+            );
+            modal.hide();
+          } else {
+            alert("Failed to reset password. Please try again.");
+            modal.hide();
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          alert("Failed to reset password. Please try again.");
         });
-    } else {
-        console.error('Could not find loginForm element.');
-    }
+    });
 });
